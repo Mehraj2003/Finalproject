f975f17e2cf5ade4264ce344b40ceca9e3f459ff
Mohd Mehraj Ahmed <mohdmehrajahmed9@gmail.com>
2023-08-02 13:47:07 +0530

    Updated to main branch

--- backend/routes/payment.js
+++
@@ -1,7 +0,0 @@
-const express=require('express')
-const makePayment = require('../controllers/payment/payment_controller')
-const router=express.Router()
-
-router.post('/makepayment',makePayment);
-
-module.exports=router
